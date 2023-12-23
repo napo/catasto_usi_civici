f7f6c482274380c0a6f49d5bce70f60e04b8d060
napo <napo@fbk.eu>
2023-12-23 13:20:58 +0100

    typo

--- code/usi_civici.py
+++ code/usi_civici.py
@@ -9,7 +9,7 @@ import zipfile
 dest_doc = "docs"
 donwload_url = "https://catastotn.tndigit.it/export_semestrale_VL_PUBB/IDR0020230701_TIPOCATSH_CCXXX.zip"
 url_csv = "https://docs.google.com/spreadsheets/d/e/2PACX-1vSPeLuWTTF1JhWOhhR_ZJmSLBJhMqcJ771xWUeNnuX2co7aV2k2UytMRWU3AZdgfP4gIsWZZHsmx3T7/pub?output=csv"
-src_comunicatastaliamministrativi = "code' + os.sep + "comuni_catastali_amministrativi_trentino.csv"
+src_comunicatastaliamministrativi = "code" + os.sep + "comuni_catastali_amministrativi_trentino.csv"
 comunicatastaliamministrativi = pd.read_csv(src_comunicatastaliamministrativi)
 
 def getComuneAmministrativo(name):
