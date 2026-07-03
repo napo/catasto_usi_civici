e1289c6234145f55900954b96263d9c271b2a4e7
Maurizio Napolitano <napo@fbk.eu>
2026-07-03 23:28:33 +0200

    Update usi_civici.py

--- code/usi_civici.py
+++ code/usi_civici.py
@@ -11,7 +11,7 @@ import requests
 import zipfile
 # dest_doc = ".." + os.sep + "docs"
 dest_doc = "docs"
-datepublished = "20251231"
+datepublished = "20260701"
 download_url = "https://catastotn.tndigit.it/export_semestrale_VL_PUBB/IDR00" + datepublished + "_TIPOCATSH_CCXXX.zip"
 url_csv = "https://docs.google.com/spreadsheets/d/e/2PACX-1vSPeLuWTTF1JhWOhhR_ZJmSLBJhMqcJ771xWUeNnuX2co7aV2k2UytMRWU3AZdgfP4gIsWZZHsmx3T7/pub?output=csv"
 src_comunicatastaliamministrativi = "comuni_catastali_amministrativi_trentino.csv"
